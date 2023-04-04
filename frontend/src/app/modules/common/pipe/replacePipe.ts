c2f4d7cdf782ac432cde66496f9ecd0bb1fbb6e9
Krzysztof Kosmala <kkosmala9@gmail.com>
2023-04-04 23:59:31 +0200

    added payment to order and pipe

--- frontend/src/app/modules/common/pipe/replacePipe.ts
+++ frontend/src/app/modules/common/pipe/replacePipe.ts
@@ -1,9 +1,14 @@
 import { PipeTransform } from "@angular/core";
+import { serialize } from "v8";
 
 export class ReplacePipe implements PipeTransform
 {
     transform(value: string, strToReplace: string, replacementStr: string) {
-        throw new Error("Method not implemented.");
+        if(!value || !strToReplace || !replacementStr)
+        {
+            return value;
+        }
+        return value.replace(new RegExp(strToReplace, 'g'), replacementStr)
     }
 
 }
